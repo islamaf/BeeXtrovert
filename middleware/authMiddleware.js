3bce367c1ceaaf628f7c4236f598b4b49bd1b8b2
islamaf <islam_9t9@yahoo.com>
2021-10-30 22:53:24 +0300

    add implementation of admin panel

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,10 @@
-const User = require('../models/User')
+const User = require('../models/User.js')
+let admin_array = ["islam", "marvin", "Lordhomie"];
 
 module.exports = (req, res, next) => {
-    User.findById(req.session.userId, (error, user ) =>{
-        if(error || !user ){
+    User.findOne({isAdmin: true}, (error, user ) =>{
+        if(!admin_array.includes(req.session.userName) || error || !user ){
+            console.log("User not authorized as admin.");
             return res.redirect('/');
         }
 
--- models/User.js
+++ models/User.js
@@ -11,6 +11,14 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 });
 
